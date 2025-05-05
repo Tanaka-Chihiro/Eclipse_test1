07d845aeec30d3748cca91cfd8be3dd2743a4d2b
Tanaka Chihiro <tokyo.9st@gmail.com>
2025-05-05 18:18:31 +0900

    Initial commit

---
+++ exercise/src/exercise/First.java
@@ -0,0 +1,18 @@
+package exercise;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("Eclipseを始めよう！");
+		System.out.println(5+9);
+		
+		int number1 = 40;
+		int number2 = 8;
+		
+		System.out.println(number1 + number2);
+		System.out.println(number1 - number2);
+		System.out.println(number1 * number2);
+		System.out.println(number1 / number2);
+	}
+
+}
